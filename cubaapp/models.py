af2391bb5b2e18272f1bda14a3e68b10fdf44e5a
jeanian26 <paulian829@gmail.com>
2022-12-21 20:36:05 +0800

    update

--- cubaapp/models.py
+++ cubaapp/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class User(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     fname = models.CharField(max_length=30)
     lname = models.CharField(max_length=30)
     email = models.EmailField(unique=True)
